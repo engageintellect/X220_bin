c144d479ff3f25e0f062edcc668b6b463ca3e89e
jc9361 <jc9361@gmail.com>
2021-01-14 01:31:22 -0800

    updating

--- env/scripts/surf/rofi_google_search.sh
+++ env/scripts/surf/rofi_google_search.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 BROWSER=surf
-read selection <<< $(rofi -dmenu -p "SEARCH" -font "Hack Nerd font 10" -location 0 -width 20 -lines 10)
+# read selection <<< $(rofi -dmenu -p "SEARCH" -font "Hack Nerd font 10" -location 1 -width 20 -lines 100 -m 1)
+read selection <<< $(rofi -dmenu -p "SEARCH" -font "Hack Nerd font 10" -location 0 -width 25 -lines 10)
 
 
 
--- env/scripts/surf/rofi_url_surf.sh
+++ env/scripts/surf/rofi_url_surf.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 BROWSER=surf
-read selection <<< $(rofi -dmenu -p "URL" -font "Hack Nerd font 10" -location 0 -width 20 -lines 10)
+read selection <<< $(rofi -dmenu -p "URL" -font "Hack Nerd font 10" -location 0 -width 25 -lines 10)
 
 
 
